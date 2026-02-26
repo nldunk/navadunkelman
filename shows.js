b6c00fb3d734382d45613d9aac644f7ce6be8d14
nldunk <nava.dunkelman@gmail.com>
2026-02-26 00:04:04 -0500

    show update

--- shows.js
+++ shows.js
@@ -25,6 +25,12 @@ const showsData = {
             location: "The Stone, New York, NY",
         },
         {
+            date: "April 3",
+            time: "8:30PM",
+            title: "Trio with Tomeka Reid and Ikue Mori",
+            location: "The Stone, New York, NY",
+        },
+        {
             date: "September 4",
             time: "8:30PM",
             title: "Trio with Erik Friedlander and Myk Freedman",
